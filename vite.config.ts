f7ead331cb2a9aba3e76f966998166d1ce5a9537
Gonzalo Miguel <gonzalo.augustin.miguel@gmail.com>
2025-10-03 19:10:52 -0300

    Cambios2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para GitHub Pages
+  base: '/', // Configurado para Vercel
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
